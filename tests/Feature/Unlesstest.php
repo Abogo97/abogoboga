ee627ec9dbf61ac1567932b0e2cd2aefbaa902ab
Abogo97 <ikhwankrasak123@gmail.com>
2025-04-20 13:28:35 +0700

    unless

---
+++ tests/Feature/Unlesstest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class Unlesstest extends TestCase
+{
+    public function test_unless()
+    {
+        $this->view('unless',['isAdmin'=> true])
+        ->assertDontSeeText('You are not admin');
+        $this->view('unless',['isAdmin'=> false])
+        ->assertSeeText('You are not admin');
+    }
+}
